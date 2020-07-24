9ae04abfd5f25f70469d1ff6b331770c43ebc552
leanfiorita <lean.fiorita96@gmail.com>
2020-07-24 03:13:56 -0300

    Automouse 0.1

---
+++ Autoclick/Autoclick/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autoclick
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.hazardmu.tk");
+        }
+
+        private void Main_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // Capture F5 KeyPress
+            string Tecla1 = comboStop.Text.ToString();
+            if (e.Key == Key.F5)
+            {
+                System.Windows.MessageBox.Show("F5 Key Pressed!");
+                e.Handled = true;
+            }
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown += Main_KeyDown;
+        }
+
+        public class MouseOperations
+        {
+            [Flags]
+            public enum MouseEventFlags
+            {
+                LeftDown = 0x00000002,
+                LeftUp = 0x00000004,
+                MiddleDown = 0x00000020,
+                MiddleUp = 0x00000040,
+                Move = 0x00000001,
+                Absolute = 0x00008000,
+                RightDown = 0x00000008,
+                RightUp = 0x00000010
+            }
+
+            [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool SetCursorPos(int x, int y);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+            [DllImport("user32.dll")]
+            private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+            public static void SetCursorPosition(int x, int y)
+            {
+                SetCursorPos(x, y);
+            }
+
+            public static void SetCursorPosition(MousePoint point)
+            {
+                SetCursorPos(point.X, point.Y);
+            }
+
+            public static MousePoint GetCursorPosition()
+            {
+                MousePoint currentMousePoint;
+                var gotPoint = GetCursorPos(out currentMousePoint);
+                if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+                return currentMousePoint;
+            }
+
+            public static void MouseEvent(MouseEventFlags value)
+            {
+                MousePoint position = GetCursorPosition();
+
+                mouse_event
+                    ((int)value,
+                     position.X,
+                     position.Y,
+                     0,
+                     0)
+                    ;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MousePoint
+            {
+                public int X;
+                public int Y;
+
+                public MousePoint(int x, int y)
+                {
+                    X = x;
+                    Y = y;
+                }
+            }
+        }
+
+    }
+}
